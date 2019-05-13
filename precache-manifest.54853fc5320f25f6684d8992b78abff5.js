acfc8c5e1fcdbac45b0a5fea15001faf161d506b
raydwaipayan <dwaipayanray1@gmail.com>
2019-05-13 22:33:44 +0530

    Updates

---
+++ precache-manifest.54853fc5320f25f6684d8992b78abff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f78fcb63c8804e5a65fe8a5d59eb6e",
+    "url": "/redux-notes/index.html"
+  },
+  {
+    "revision": "85f01dc3146e5d775566",
+    "url": "/redux-notes/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6a02ef070e2df4401450",
+    "url": "/redux-notes/static/js/2.f1779b37.chunk.js"
+  },
+  {
+    "revision": "85f01dc3146e5d775566",
+    "url": "/redux-notes/static/js/main.63fd12de.chunk.js"
+  },
+  {
+    "revision": "4cf38cf990ca2559687f",
+    "url": "/redux-notes/static/js/runtime~main.ea62ceb8.js"
+  }
+]);
